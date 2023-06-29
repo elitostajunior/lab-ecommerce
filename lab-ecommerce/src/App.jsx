08c786660ab65b837bfb592cc1bf597b36da0512
eligtj <eli_gtj@hotmail.com>
2023-06-29 17:26:39 -0300

    componente Filtros finalizado

--- lab-ecommerce/src/App.jsx
+++ lab-ecommerce/src/App.jsx
@@ -7,47 +7,52 @@ class App extends React.Component {
     filtroMinimo: 20,
     filtroMaximo: 10000,
     filtroBuscaPorNome: "busca"
-  }
+  };
 
   manipularValorDoFiltroMinimo = (event) => {
     this.setState({
       filtroMinimo: event.target.value
-    })
-  }
+    });
+  };
 
   manipularValorDoFiltroMaximo = (event) => {
     this.setState({
       filtroMaximo: event.target.value
-    })
-  }
+    });
+  };
 
   manipularValorDoFiltroBuscaPorNome = (event) => {
     this.setState({
       filtroBuscaPorNome: event.target.value
-    })
-  }
+    });
+  };
 
-  render() {
-    const pacotesFiltradosMinimo = pacoteDeProdutos.filter(produto => {
+  filtrarProdutos = () => {
+    const pacotesFiltradosMinimo = pacoteDeProdutos.filter((produto) => {
       if(this.state.filtroMinimo){
         return produto.price >= this.state.filtroMinimo
       }
-    })
+    });
 
-    const pacotesFiltradosMaximo = pacoteDeProdutos.filter(produto => {
+    const pacotesFiltradosMaximo = pacotesFiltradosMinimo.filter((produto) => {
       if(this.state.filtroMaximo){
         return produto.price <= this.state.filtroMaximo
       } else {
         return produto
       }
-    })
+    });
 
-    const pacotesFiltradosBuscaPorNome = pacoteDeProdutos.filter(produto => {
+    const pacoteFiltrado = pacotesFiltradosMaximo.filter((produto) => {
       return produto.name.includes(this.state.filtroBuscaPorNome)
-    })
+    });
 
-    console.log(pacotesFiltradosBuscaPorNome)
+    return pacoteFiltrado
+  };
 
+  render() {
+
+    const produtosFiltrados = this.filtrarProdutos()
+    console.log(produtosFiltrados)
 
     return (
       <div>
