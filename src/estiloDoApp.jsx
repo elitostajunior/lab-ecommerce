6bb00db66f8f4484a5daa8a2fdc4af30b0504b2b
eligtj <eli_gtj@hotmail.com>
2023-08-06 11:57:27 -0300

    alteração na fonte

--- src/estiloDoApp.jsx
+++ src/estiloDoApp.jsx
@@ -1,6 +1,7 @@
 import { styled } from "styled-components";
 
 export const ConjuntoDeComponentes = styled.div`
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
   display: grid;
   grid-template-columns: 1fr 2fr 1fr;
   gap: 16px;
